9170b60acaaf888750aa48243f2645a64155350c
einrocker <einrocker@googlemail.com>
2021-10-23 22:51:42 +0200

    added some more entities inside KotlinPattern.kt - file will be renamed asap - added a first rough sketch of a game loop - added a very rough sketch of how to implement the timing system

--- src/main/kotlin/de/amirrocker/functional/KotlinPattern.kt
+++ src/main/kotlin/de/amirrocker/functional/KotlinPattern.kt
@@ -1,5 +1,13 @@
 package de.amirrocker.functional
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 // TODO See 90 of 346 - - patterns with kotlin
 /**
  * We use the typealias construct to express the actual domain
@@ -9,6 +17,7 @@ package de.amirrocker.functional
 typealias WeaponDamage = Int
 typealias DistanceTraveled = Int
 typealias SupportGiven = Int
+typealias ResearchDone = Int
 typealias Longitude = Long
 typealias Latitude = Long
 
@@ -30,6 +39,8 @@ const val REGULAR_LEGS_TRAVELING_DISTANCE : DistanceTraveled = 50
 const val DEFAULT_LEAP_FROG_DISTANCE = 500
 
 
+
+
 /**
  * Each entity is represented as a Unit
  */
@@ -43,6 +54,14 @@ interface Unit {
  */
 interface Support : Unit {
     fun support():SupportGiven
+}
+
+interface ScienceUnit : Unit {
+    override fun attack(longitude: Longitude, latitude: Latitude) {
+        println("ScienceUnit does minimal damage of 5")
+    }
+
+    fun research():ResearchDone
 }
 
 /**
@@ -63,7 +82,7 @@ interface Legs {
     fun move():DistanceTraveled
 }
 
-sealed interface Wheels : Legs {
+interface Wheels : Legs {
     object DEFAULT_MOTOR : Wheels {
         override fun move(): DistanceTraveled = DEFAULT_LEAP_FROG_DISTANCE // the default travel distance for wheeled units
     }
@@ -80,6 +99,12 @@ enum class InfantryUnits {
     Robocops
 }
 
+enum class ResearchUnits {
+    Researcher,
+    LaboratoryTech,
+    FieldOperative
+}
+
 enum class MechanizedUnits {
     TANK,
     APC,
@@ -87,6 +112,7 @@ enum class MechanizedUnits {
     SPAAG,
     SUPPLY, // support Unit
 }
+
 
 class Soldier(
     private val weapon:Weapon,
@@ -115,6 +141,22 @@ class Soldier(
     }
 }
 
+// TODO add other gadgets
+class Scientist(
+    val legs: Legs
+) : ScienceUnit, Infantry {
+
+    override fun move(longitude: Longitude, latitude: Latitude) {
+        val distanceTraveled = legs.move()
+        println("Soldier moved: $distanceTraveled m")
+    }
+
+    override fun research(): ResearchDone {
+        println("no research done - not yet implemented")
+        return 0
+    }
+}
+
 /**
  * The Vehicle classes
  */
@@ -140,12 +182,12 @@ class Tank(
 ) : Vehicle {
     override fun attack(longitude: Longitude, latitude: Latitude) {
         val damagedCaused = weapon.causeDamage()
-        println("APC attacked enemy for damage: $damagedCaused")
+        println("TANK attacked enemy for damage: $damagedCaused")
     }
 
     override fun move(longitude: Longitude, latitude: Latitude) {
         val distanceTraveled = wheels.move()
-        println("APC moved: $distanceTraveled m")
+        println("TANK moved: $distanceTraveled m")
     }
 }
 
@@ -186,10 +228,6 @@ val grenadier = Soldier(GrenadeLauncher(), RegularLegs())
 val robosoldier = Soldier(GrenadeLauncher(), ExoSkeletonLegs())
 // TEST ONLY - use Barracks to create units
 
-
-
-
-
 /**
  * sweet - now lets start adding some buildings
  */
@@ -197,36 +235,40 @@ interface Headquarters {
     val buildings:List<Building<*, Unit>>
 }
 
+class RegimentalHeadquarter : Headquarters {
 
+    override val buildings: List<Building<*, Unit>> by lazy {
+            mutableListOf()
+    }
 
+    fun buildBarracks() : Barracks = Barracks().also { building -> (buildings as MutableList).add(building) }
 
-//class RegimentalHeadquarter : Headquarters {
-//
-////    override val buildings: List<Building<*, Unit>> by lazy {
-////            MutableList<Building<*, Unit>>(0) { Building("some", "",
-////                Soldier( M4AssaultRifle(), AthleticLegs() )
-////            )
-////        }
-////    }
-////        get() = field
-//
-//
-//    fun buildBarracks() : Barracks = Barracks("someName").also { building -> (buildings as MutableList).add(building) }
-//
-//    fun buildVehicleFactory(): VehicleFactory = VehicleFactory("factoryName")
-//
-//}
+    fun buildVehicleFactory(): VehicleFactory = VehicleFactory().also { factory -> (buildings as MutableList).add(factory) }
 
-//data class VehicleFactory(
-//    val factoryName:FactoryName
-//) {
-//
-//}
+}
 
 interface Building<in UnitType, out ProducedUnit> where UnitType : Enum<*>, ProducedUnit : Unit {
     fun build(type:UnitType):ProducedUnit
 }
 
+interface ScienceBuilding<in UnitType, out ProducedUnit> where UnitType : Enum<*>, ProducedUnit : ScienceUnit {
+    fun build(type:UnitType):ProducedUnit
+}
+
+
+sealed class ResearchCenter : ScienceBuilding<ResearchUnits, Scientist> {
+    override fun build(type: ResearchUnits): Scientist =
+        when(type) {
+            ResearchUnits.Researcher -> buildResearcher()
+            ResearchUnits.FieldOperative -> buildFieldOperative()
+            ResearchUnits.LaboratoryTech -> buildLaboratoryTech()
+        }
+
+    private fun buildResearcher(): Scientist = Scientist(AthleticLegs())
+    private fun buildFieldOperative(): Scientist = Scientist(AthleticLegs())
+    private fun buildLaboratoryTech(): Scientist = Scientist(AthleticLegs())
+
+}
 
 class Barracks : Building<InfantryUnits, Infantry> {
     override fun build(type: InfantryUnits): Infantry =
@@ -237,42 +279,123 @@ class Barracks : Building<InfantryUnits, Infantry> {
             InfantryUnits.Robocops -> buildRobocops()
         }
 
-    fun buildRanger() = Soldier(M4AssaultRifle(), RegularLegs())
-    fun buildGrenadiers() = Soldier(GrenadeLauncher(), AthleticLegs())
-    fun buildMedics() = Soldier(M4AssaultRifle(), ExoSkeletonLegs())
-    fun buildRobocops() = Soldier(GrenadeLauncher(), ExoSkeletonLegs())
+    private fun buildRanger() = Soldier(M4AssaultRifle(), RegularLegs())
+    private fun buildGrenadiers() = Soldier(GrenadeLauncher(), AthleticLegs())
+    private fun buildMedics() = Soldier(M4AssaultRifle(), ExoSkeletonLegs())
+    private fun buildRobocops() = Soldier(GrenadeLauncher(), ExoSkeletonLegs())
 }
-
 
 class VehicleFactory : Building<MechanizedUnits, Vehicle> {
 
     override fun build(type: MechanizedUnits): Vehicle =
         when(type) {
-            MechanizedUnits.APC ->
-            MechanizedUnits.IFV ->
-            MechanizedUnits.SPAAG ->
-            MechanizedUnits.TANK ->
-            MechanizedUnits.SUPPLY ->
+            MechanizedUnits.APC -> createAPC()
+            MechanizedUnits.IFV -> createIFV()
+            MechanizedUnits.SPAAG -> createSPAAG()
+            MechanizedUnits.TANK -> createTank()
+            MechanizedUnits.SUPPLY -> createSupply()
         }
 
+    private fun createAPC(): APC = APC(GrenadeLauncher())
+
+    private fun createIFV(): APC = APC(GrenadeLauncher()) // TODO give IFV a TOW missile
+
+    private fun createSPAAG(): APC = APC(GrenadeLauncher()) // TODO a 4 x Barrel 0.5 Cal :)
+
+    private fun createTank(): Tank = Tank(GrenadeLauncher()) // TODO a 110 mm smoothbore FSAPDS / HEATFS :) :)
+
+    private fun createSupply(): APC = APC(M4AssaultRifle()) // TODO think on how to represent supply and training
 
 }
 
+typealias NumberOfColumns = Int
+typealias NumberOfRows = Int
 
+interface Map {
 
+    data class Board(
+        val columns:NumberOfColumns,
+        val rows:NumberOfRows,
+    ) : Map {
+        override val board: Board
+            get() = this
+    }
+
+    val board:Board
+
+    fun getSize() = board.columns * board.rows
+}
 
 
 
 // test method
-fun createSoldierGame(vararg args:String) {
+fun createSoldierGame(vararg args:String) = runBlocking {
 
-    rifleman.attack(Longitude.MIN_VALUE, Latitude.MIN_VALUE)
-    grenadier.move(Longitude.MIN_VALUE, Latitude.MIN_VALUE)
-    robosoldier.attack(Longitude.MIN_VALUE, Latitude.MIN_VALUE)
+        // coroutine Builder
+        launch {
+
+            // special suspend function
+            delay(500)
+
+            println(
+//                 suspend function
+                simpleSequence().forEach { println(it) }
+            )
+        }
+
+        async {
+            startGameTime().collect { value -> println("gametime value $value") }
+        }
+
+        val board = Map.Board(64, 64)
+        println(board.getSize())
 
 
+        val barracks = RegimentalHeadquarter().buildBarracks()
+        val vehicleFactory = RegimentalHeadquarter().buildVehicleFactory()
+
+        println("$barracks created")
+        println("$vehicleFactory created")
+
+        val ranger = barracks.build(InfantryUnits.Rangers)
+        ranger.move(0L, 0L)
+        ranger.attack(0L, 0L)
+
+        val tank = vehicleFactory.build(MechanizedUnits.TANK)
+        tank.move(0L, 0L)
+        tank.attack(0L, 0L)
+
+        launch {
+            simulateSimpleAIMoves(tank).collect { value -> println("AI moved $value") }
+        }
+    }
+
+
+
+
+fun startGameTime():Flow<Int> = flow {
+    (0 until 60).forEach {
+        delay(1000)
+        emit(it)
+    }
 }
 
+fun simulateSimpleAIMoves( tank:Vehicle ):Flow<Int> = flow {
+    (0 until 60).forEach {
+        delay(1000)
+        tank.move(0L, 0L)
+        emit(it)
+    }
+}
+
+
+
+// playing with flow
+
+suspend fun simpleSequence() = sequence { (0..5).forEach {
+//    Thread.sleep(2000)
+    yield(it)
+} }
 
 
 
